0f5c163667f0455a6a0d3d9205239d4cecf12f30
xavcaron <70541307+xavcaron@users.noreply.github.com>
2020-09-09 10:41:21 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        chr(ord(lettre)+32)
+        chr(ord(lettre)-32)
         resultat += lettre
     return resultat
 
